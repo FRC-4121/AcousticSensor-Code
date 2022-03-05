6354785f2f48337176c78ac61566355a4b093fca
AndrewSelinsky <95663139+AndrewSelinsky@users.noreply.github.com>
2022-03-05 13:44:03 -0500

    Updated Acoustic Code
    Final Version Of Acoustic Code

--- AcousticSensor/Acoustic-Sensor-2022/src/main/java/extraClasses/AcousticSensor.java
+++ AcousticSensor/Acoustic-Sensor-2022/src/main/java/extraClasses/AcousticSensor.java
@@ -1,6 +1,25 @@
 package extraClasses;
-import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.math.filter.MedianFilter;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class AcousticSensor {
-    Ultrasonic sensor = new Ultrasonic(1, 2);
+    
+    private final AnalogInput sensor = new AnalogInput(0); //Change the number to correspond to the port it is in
+    MedianFilter filter = new MedianFilter(100);
+    
+    // Returns inches
+    public double getValue(){
+        double rawValue = sensor.getValue();
+        double voltage_scale_factor = 5/RobotController.getVoltage5V();
+        double currentDistanceInches = rawValue * voltage_scale_factor * 0.0492;
+        filter.calculate(currentDistanceInches);
+        return currentDistanceInches;
+    }
+
+    public void displayValues(){
+        double currentDistanceInches = getValue();
+        SmartDashboard.putNumber("inches", currentDistanceInches);
+    }
 }
